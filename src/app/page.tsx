87e62d5dc562de07a24aa0b7471ed5cc14b3872f
judestp <igot.judefrancis@seedtech.ph>
2025-10-21 16:21:09 +0800

    chore: support tailwind and scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,67 @@
 'use client';
 
 import { useState } from 'react';
+import '@/styles/scss/main.scss';
+import Login from '@/components/Login';
+import User from '@/components/User';
 
-function Home() {
-  const [count, setCount] = useState(0);
+const pages = {
+  Login,
+  User,
+} as const;
+
+type PageKey = keyof typeof pages;
+
+function App() {
+  const [currentPage, setCurrentPage] = useState<PageKey>('Login');
+
+  const isKeyOf = <T extends object>(
+    obj: T,
+    key: string,
+  ): key is Extract<keyof T, string> => {
+    return key in obj;
+  };
+
+  const handleNavigation = (page: PageKey) => {
+    setCurrentPage(page);
+  };
+
+  const CurrentPageComponent = pages[currentPage];
 
   return (
-    <div style={{ zoom: '500%', textAlign: 'center' }}>
-      <h1>JTB Front End</h1>
-      <div>
-        <button
-          onClick={(): void => {
-            setCount((count: number) => {
-              return count + 1;
-            });
-          }}
-        >
-          count is {count}
-        </button>
+    <div className="grid grid-cols-[auto_1fr] h-screen">
+      <div className="border-r border-gray-300 p-4 bg-gray-100 w-auto flex flex-col justify-between">
+        <div>
+          {Object.keys(pages).map((page) => {
+            if (isKeyOf(pages, page)) {
+              return (
+                <div
+                  key={page}
+                  className={`cursor-pointer p-2 hover:bg-gray-200 font-bold ${
+                    currentPage === page ? 'bg-gray-200' : ''
+                  }`}
+                  onClick={() => {
+                    handleNavigation(page);
+                  }}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={() => null}
+                  aria-label={page}
+                >
+                  {page.charAt(0).toUpperCase() + page.slice(1)}
+                </div>
+              );
+            }
+            return null;
+          })}
+        </div>
+      </div>
+
+      <div className="p-8">
+        <CurrentPageComponent />
       </div>
     </div>
   );
 }
 
-export default Home;
+export default App;
---
+++ src/components/User.tsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <h1 className="text-2xl font-bold">User</h1>;
+}
+
+export default Login;
